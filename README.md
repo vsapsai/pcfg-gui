bd2f41be3c0d14537305b0c44af97574e120f3e1
Volodymyr Sapsai <vsapsai@gmail.com>
2012-08-24 14:10:50 +0300

    Initial version.

---
+++ README.md
@@ -0,0 +1,1 @@
+Browser based GUI for competitive grammar writing http://old-site.clsp.jhu.edu/grammar-writing/
---
+++ server.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import http.server
+
+class HTTPRequestHandler(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == "/":
+            self.send_file("index.html")
+        else:
+            self.send_not_found_response()
+        # self.send_response(200)
+        # self.wfile.write(b"abc\n")
+
+    def send_file(self, filename):
+        try:
+            with open(filename, "rb") as f:
+                self.send_response(200)
+                self.send_header("Content-Type", "text/html;charset=utf-8")
+                self.send_header("Connection", "close")
+                self.end_headers()
+
+                file_contents = f.read()
+                self.wfile.write(file_contents)
+        except IOError:
+            self.send_not_found_response()
+
+    def send_not_found_response(self):
+        self.send_error(404)
+
+def run(server_class=http.server.HTTPServer, handler_class=http.server.BaseHTTPRequestHandler):
+    server_address = ('', 8000)
+    httpd = server_class(server_address, handler_class)
+    print("Server started...")
+    httpd.serve_forever()
+
+run(handler_class=HTTPRequestHandler)
