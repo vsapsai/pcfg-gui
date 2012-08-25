e1703cf9b49f684f55897bab0636b482701342bb
Volodymyr Sapsai <vsapsai@gmail.com>
2012-08-25 15:20:40 +0300

    Added support for PUT.

--- server.py
+++ server.py
@@ -4,6 +4,10 @@ import cgi, urllib.parse
 import json
 import re
 
+class HTTPStatusCode:
+    SUCCESS = 200
+    NOT_IMPLEMENTED_ERROR = 501
+
 class HTTPRequestHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path == "/":
@@ -13,19 +17,14 @@ class HTTPRequestHandler(http.server.BaseHTTPRequestHandler):
             self.send_file(relative_file_path)
         else:
             self.send_not_found_response()
-        # self.send_response(200)
-        # self.wfile.write(b"abc\n")
 
     def do_POST(self):
         if self.path == "/app/items":
-            content_type = self.headers.get_content_type()
-            if content_type != "application/json":
-                self.send_error(415) # Unsupported Media Type
+            obj = self.JSON_from_request()
+            if obj is None:
+                self.send_error(HTTPStatusCode.NOT_IMPLEMENTED_ERROR)
                 return
 
-            content = self.rfile_content()
-            content = str(content, "utf-8")
-            obj = json.loads(content)
             if "content" in obj:
                 obj["length"] = len(obj["content"])
                 del obj["content"]
@@ -33,14 +32,38 @@ class HTTPRequestHandler(http.server.BaseHTTPRequestHandler):
 
             self.send_response(201)
             self.send_header("Location", "/app/items/1")
+            self.send_header("Content-Type", "application/json")
             self.send_header("Connection", "close")
             self.end_headers()
 
             self.wfile.write(json.dumps(obj).encode())
         else:
-            self.send_error(501)
+            self.send_error(HTTPStatusCode.NOT_IMPLEMENTED_ERROR)
+
+    def do_PUT(self):
+        if re.match("^/app/items/", self.path):
+            obj = self.JSON_from_request()
+            if obj is None:
+                self.send_error(HTTPStatusCode.NOT_IMPLEMENTED_ERROR)
+                return
+
+            if "content" in obj:
+                obj["length"] = len(obj["content"])
+                del obj["content"]
+
+            self.send_response(HTTPStatusCode.SUCCESS)
+            self.send_header("Content-Type", "application/json")
+            self.send_header("Connection", "close")
+            self.end_headers()
+            self.wfile.write(json.dumps(obj).encode())
+        else:
+            self.send_error(HTTPStatusCode.NOT_IMPLEMENTED_ERROR)
 
     def rfile_content(self):
+        """"Reads from rfile Content-Length bytes.
+
+        There is a catch if you read too much, connection won't be closed.
+        That's why need to read exactly Content-Length bytes."""
         length = self.headers["Content-Length"]
         if length is None:
             return None
@@ -48,12 +71,22 @@ class HTTPRequestHandler(http.server.BaseHTTPRequestHandler):
         content = self.rfile.read(length)
         return content
 
+    def JSON_from_request(self):
+        "Tries to interpret rfile as JSON and returns deserialized JSON."
+        content_type = self.headers.get_content_type()
+        if content_type != "application/json":
+            return None
+        content = self.rfile_content()
+        content = str(content, "utf-8")
+        obj = json.loads(content)
+        return obj
+
     def send_file(self, filename):
         try:
             with open(filename, "rb") as f:
-                self.send_response(200)
+                self.send_response(HTTPStatusCode.SUCCESS)
                 #self.send_header("Content-Type", "text/html;charset=utf-8")
-                #self.send_header("Connection", "close")
+                self.send_header("Connection", "close")
                 self.end_headers()
 
                 file_contents = f.read()
