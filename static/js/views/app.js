576a88f87a8a1a7820ea54105442ada1d1107eb9
Volodymyr Sapsai <vsapsai@gmail.com>
2012-08-25 15:59:27 +0300

    Display length continuously.

--- static/js/views/app.js
+++ static/js/views/app.js
@@ -5,17 +5,25 @@ $(function($) {
         el: '#app',
 
         events: {
-            'click #submit': 'talkToServer'
+            'keyup #edit': 'saveModel'
         },
 
         initialize: function() {
             var item = new app.Item();
-            app.itemView = new app.ItemView({model: item});
-            $('#item-placeholder').append(app.itemView.render().el);
+            this.model = item;
+            item.on('change', this.render, this);
+            this.render();
         },
 
-        talkToServer: function() {
-            app.itemView.updateModel();
+        render: function() {
+            this.$input = this.$('#edit');
+            this.$input.val(this.model.get('content'));
+            this.$('#length').text(this.model.get('length'));
+            return this;
+        },
+
+        saveModel: function() {
+            this.model.save({content: this.$input.val()});
         }
     });
 });
